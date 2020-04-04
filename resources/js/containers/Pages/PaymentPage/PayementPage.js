170a890726c00a3801e9b5e72727551dfdfbff01
marvinboris <47755699+marvinboris@users.noreply.github.com>
2020-04-04 13:19:22 +0100

    Successfully compiled

--- resources/js/containers/Pages/PaymentPage/PayementPage.js
+++ resources/js/containers/Pages/PaymentPage/PayementPage.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react'
 import AppBar from '../../../components/AppBar/AppBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle, faChevronRight,faWallet, faDollarSign } from '@fortawesome/free-solid-svg-icons';
-import { Container, Row, Col,InputGroup, InputGroupAddon,Form, InputGroupText, Input, Button, Label, FormGroup, Spinner  } from 'reactstrap';
+import { faCheckCircle, faChevronRight, faWallet, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import { Container, Row, Col, InputGroup, InputGroupAddon, Form, InputGroupText, Input, Button, Label, FormGroup, Spinner } from 'reactstrap';
 import manpaye from '../../../assets/images/man-paye.png';
 import { connect } from 'react-redux';
 import * as actions from '../../../store/actions/index';
 
- class PayementPage extends Component {
+class PayementPage extends Component {
 
     state = {
-        loading:false,
-        processing:false,
-        processPage:<h1>process</h1>
+        loading: false,
+        processing: false,
+        processPage: <h1>process</h1>
     }
 
-    onSubmitHandler = ()=>{
-        this.setState({...this.state, loading:true});
-        fetch('https://www.monetbil.africa/pay/v2.1/gKznLEpbkBj7EOXVxx3WvH4Yw3Ijuk').then(response=>{
-        return response.text();
-    }).then(html=>{
-       
-        
-       
-        return html
-    }).then(htmlDoc=>{ console.log(htmlDoc);
-        
-         this.setState({loading:false,processing:true,processPage:<iframe style={{border:'none'}} scrolling="no" width="600px" height="670px" src='https://www.monetbil.africa/pay/v2.1/gKznLEpbkBj7EOXVxx3WvH4Yw3Ijuk'></iframe>})
-    })
-        .catch(err => console.log(err));
+    onSubmitHandler = () => {
+        this.setState({ ...this.state, loading: true });
+        fetch('https://www.monetbil.africa/pay/v2.1/gKznLEpbkBj7EOXVxx3WvH4Yw3Ijuk').then(response => {
+            return response.text();
+        }).then(html => {
+
+
+
+            return html
+        }).then(htmlDoc => {
+            console.log(htmlDoc);
+
+            this.setState({ loading: false, processing: true, processPage: <iframe style={{ border: 'none' }} scrolling="no" width="600px" height="670px" src='https://www.monetbil.africa/pay/v2.1/gKznLEpbkBj7EOXVxx3WvH4Yw3Ijuk'></iframe> })
+        })
+            .catch(err => console.log(err));
     }
 
     inputChangeHandler = (e, name) => {
@@ -36,61 +37,61 @@ import * as actions from '../../../store/actions/index';
     }
 
     render() {
-        let loading = <Spinner color="warning"/>
+        let loading = <Spinner color="warning" />
         let processing = this.state.processPage
-       let payementProcess=
+        let payementProcess =
             <Form onSubmit={this.onSubmitHandler}>
-            <InputGroup  size='lg outline-0'>
-                 <InputGroupAddon  addonType="prepend">
-                     <InputGroupText style={{border:'0'}} className="user-input bg-dark" >
-                         <FontAwesomeIcon color="#06b0b6" icon={faWallet} /><strong style={{marginLeft:'10px',fontSize:'30px'}}>|</strong> </InputGroupText>
-                 </InputGroupAddon>
-                 <Input onChange={(e) => this.inputChangeHandler(e, "payement-type")} name="payement-type" type="select" required style={{height:'65px'}} className="text-light bg-dark border-0 ">
-                     <option>Pay using Limo account</option>
-                     <option>Pay with BTC</option>
-                     <option>Mobile payment</option>
-                 </Input>
-             </InputGroup>
-             <br/>
-             <InputGroup className="text-left" size="lg">
-                 <InputGroupAddon  addonType="prepend">
-                     <InputGroupText style={{border:'none'}} className="bg-dark"><FontAwesomeIcon color="#06b0b6" icon={faDollarSign} /> <strong style={{marginLeft:'20px',fontSize:'30px'}}>|</strong></InputGroupText>
-                 </InputGroupAddon>
-                 <Input onChange={(e) => this.inputChangeHandler(e, "amount")} name="amount" required style={{height:'65px' }} type="number" className="bg-dark border-0 text-light"  placeholder="Amount to pay" />
-             </InputGroup>
-             <br/>
-             <FormGroup row>
-                 <Label className='text-light' for="checkbox2" xs={4}>OTB Method</Label>
-                 <Col xs={{ size: 8 }}>
-                     <Row>
-                     <Col xs={5} >
-                         <FormGroup check>
-                             <Label className="text-light" check>
-                             <Input required name="radio" type="radio" id="checkbox2" />{' '}
+                <InputGroup size='lg outline-0'>
+                    <InputGroupAddon addonType="prepend">
+                        <InputGroupText style={{ border: '0' }} className="user-input bg-dark" >
+                            <FontAwesomeIcon color="#06b0b6" icon={faWallet} /><strong style={{ marginLeft: '10px', fontSize: '30px' }}>|</strong> </InputGroupText>
+                    </InputGroupAddon>
+                    <Input onChange={(e) => this.inputChangeHandler(e, "payement-type")} name="payement-type" type="select" required style={{ height: '65px' }} className="text-light bg-dark border-0 ">
+                        <option>Pay using Limo account</option>
+                        <option>Pay with BTC</option>
+                        <option>Mobile payment</option>
+                    </Input>
+                </InputGroup>
+                <br />
+                <InputGroup className="text-left" size="lg">
+                    <InputGroupAddon addonType="prepend">
+                        <InputGroupText style={{ border: 'none' }} className="bg-dark"><FontAwesomeIcon color="#06b0b6" icon={faDollarSign} /> <strong style={{ marginLeft: '20px', fontSize: '30px' }}>|</strong></InputGroupText>
+                    </InputGroupAddon>
+                    <Input onChange={(e) => this.inputChangeHandler(e, "amount")} name="amount" required style={{ height: '65px' }} type="number" className="bg-dark border-0 text-light" placeholder="Amount to pay" />
+                </InputGroup>
+                <br />
+                <FormGroup row>
+                    <Label className='text-light' for="checkbox2" xs={4}>OTB Method</Label>
+                    <Col xs={{ size: 8 }}>
+                        <Row>
+                            <Col xs={5} >
+                                <FormGroup check>
+                                    <Label className="text-light" check>
+                                        <Input required name="radio" type="radio" id="checkbox2" />{' '}
                                  SMS
                              </Label>
-                         </FormGroup>
-                     </Col>
-                     <Col xs={5} >
-                         <FormGroup check>
-                             <Label className="text-light" check>
-                             <Input required name="radio" type="radio" id="checkbox2" />{' '}
+                                </FormGroup>
+                            </Col>
+                            <Col xs={5} >
+                                <FormGroup check>
+                                    <Label className="text-light" check>
+                                        <Input required name="radio" type="radio" id="checkbox2" />{' '}
                              Email
                              </Label>
-                         </FormGroup>
-                     </Col>
-                     </Row>
-                 </Col>
-             </FormGroup>
-             <br/>
-             <Button style={{backgroundColor: "#06b640"}} className="float-left h-75"  size=" w-75 ">
-                 <h3 >Continue <FontAwesomeIcon icon={faChevronRight}/><FontAwesomeIcon icon={faChevronRight}/></h3>
-             </Button>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                    </Col>
+                </FormGroup>
+                <br />
+                <Button style={{ backgroundColor: "#06b640" }} className="float-left h-75" size=" w-75 ">
+                    <h3 >Continue <FontAwesomeIcon icon={faChevronRight} /><FontAwesomeIcon icon={faChevronRight} /></h3>
+                </Button>
             </Form>
-        if(this.state.loading ) {
+        if (this.state.loading) {
             payementProcess = loading
         }
-        else if(this.state.processing){
+        else if (this.state.processing) {
             payementProcess = processing;
         }
         return (
@@ -120,7 +121,7 @@ import * as actions from '../../../store/actions/index';
                         `
                     }
                 </style>
-                <AppBar/>
+                <AppBar />
                 <Container>
                     <Row>
                         <Col xs={1}>
@@ -129,25 +130,25 @@ import * as actions from '../../../store/actions/index';
                         <Col className=" home-page mb-5" xs={5}>
                             <h4 className="text-left text-light"> Well done !  Please make payment and proceed to next step</h4>
                             <div className="inderline">
-                                <hr className="bg-secondary"/>
+                                <hr className="bg-secondary" />
                                 <div className="dot-warning bg-warning rounded-circle position-relative"></div>
                             </div>
                         </Col>
                         <Col xs={12}>
-                           <Row>
-                               <Col xs={5}>
+                            <Row>
+                                <Col xs={5}>
                                     {payementProcess}
-                               </Col>
+                                </Col>
 
-                               <Col xs={1}>
+                                <Col xs={1}>
                                     <div className="paye-v-line float-left"></div>
-                                </Col>                       
-                               <Col className="float-right" xs={6}>
+                                </Col>
+                                <Col className="float-right" xs={6}>
                                     <img className="mancompute float-right img-fluid" alt="mancompute" src={manpaye} />
-                              </Col>
-                           </Row>
-                           
-                       </Col>
+                                </Col>
+                            </Row>
+
+                        </Col>
                     </Row>
                 </Container>
             </div>
@@ -167,7 +168,7 @@ const mapDispatchToProps = dispatch => {
     return {
         onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/')),
         getPayementPage: () => dispatch(actions.getPayementPage)
-        
+
     };
 };
 
